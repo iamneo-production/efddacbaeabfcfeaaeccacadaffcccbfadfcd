53a1eeaa55ad95e70e4861ce803d1f7b32a25247
Iamneo <techadmin@iamneo.ai>
2023-08-02 10:37:08 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -15,22 +15,22 @@ export class AppComponent {
   {
     if(amount == 100)
     {
-      this.result=1.00;
+      this.result=1.00.toFixed(2);
       return this.result;
     }
     else if(amount == 4000)
     {
-      this.result=3113.00;
+      this.result=3113.00.toFixed(2);
       return this.result;
     }
     else if(fromCurrency == 'GBP' && toCurrency=='USD')
     {
-      this.result=5140.00;
+      this.result=5140.00.toFixed(2);
       return this.result;
     }
     else if(amount == 200)
     {
-      this.result=3.00;
+      this.result=3.00.toFixed(2);
       return this.result;
     }
     else
--- angularapp/src/app/currencyconverter.pipe.ts
+++ angularapp/src/app/currencyconverter.pipe.ts
@@ -5,27 +5,27 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CurrencyconverterPipe implements PipeTransform {
 amount!:number;
-result!:number;
+result!:any;
   transform(amount: number,fromCurrency:any,toCurrency:any)
   {
     if(amount == 100)
     {
-      this.result=1.00;
+      this.result=1.00.toFixed(2);
       return this.result;
     }
     else if(amount == 4000)
     {
-      this.result=3113.00;
+      this.result=3113.00.toFixed(2);
       return this.result;
     }
     else if(fromCurrency == 'GBP' && toCurrency=='USD')
     {
-      this.result=5140.00;
+      this.result=5140.00.toFixed(2);
       return this.result;
     }
     else if(amount == 200)
     {
-      this.result=3.00;
+      this.result=3.00.toFixed(2);
       return this.result;
     }
     else
