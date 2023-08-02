0479d820c6f9105c7043e0453c3f7a332553d93c
Iamneo <techadmin@iamneo.ai>
2023-08-02 12:44:16 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -7,30 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
-  amount!:number;
-  fromCurrency:string ='USD';
-  toCurrency:string = 'USD';
-  resValue:any;
-  submitButton(fromCurrency:any,toCurrency:any)
-  {
-    if(fromCurrency == 'INR' && toCurrency=='USD')
-    {
-      this.resValue=1.00.toFixed(2);
-      return this.resValue;
-    }
-    else if(fromCurrency == 'USD' && toCurrency=='GBP')
-    {
-      this.resValue=((0.876893/1.126735)*this.amount).toFixed(2);
-      return this.resValue;
-    }
-    else if(fromCurrency == 'GBP' && toCurrency=='USD')
-    {
-      this.resValue=((1.126735/0.876893)*this.amount).toFixed(2);
-      return this.resValue;
-    }
-    else
-    {
-      return "invaild";
-    }
-  }
 }
