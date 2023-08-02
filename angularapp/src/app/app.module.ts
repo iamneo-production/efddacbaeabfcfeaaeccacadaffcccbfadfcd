2673820124cf2f47a5d1f3dd82fd67eb354ccc3a
Iamneo <techadmin@iamneo.ai>
2023-08-02 10:12:03 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CurrencyconverterPipe } from './currencyconverter.pipe';
@@ -12,7 +12,8 @@ import { CurrencyconverterPipe } from './currencyconverter.pipe';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularapp/src/app/currencyconverter.pipe.ts
+++ angularapp/src/app/currencyconverter.pipe.ts
@@ -4,9 +4,28 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'currencyconverter'
 })
 export class CurrencyconverterPipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+amount!:number;
+result!:number;
+  transform(amount: number,fromCurrency:any,toCurrency:any)
+  {
+    if(fromCurrency == 'INR' && toCurrency=='USD')
+    {
+      this.result=(79.677056/1.126735)*this.amount;
+      return this.result;
+    }
+    else if(fromCurrency == 'USD' && toCurrency=='GBP')
+    {
+      this.result=(1.126735/0.876893)*this.amount;
+      return this.result;
+    }
+    else if(fromCurrency == 'GBP' && toCurrency=='USD')
+    {
+      this.result=(0.876893/1.126735)*this.amount;
+      return this.result;
+    }
+    else
+    {
+      return "invaild";
+    }
   }
-
 }
