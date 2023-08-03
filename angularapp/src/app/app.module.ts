ecfa855ad5e9237d3d3b9bdee5162f8a00b53337
Iamneo <techadmin@iamneo.ai>
2023-08-03 06:19:21 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -2,19 +2,8 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule} from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
-
-
 
 @NgModule({
   declarations: [
@@ -22,16 +11,9 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    MatInputModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatMenuModule,
     FormsModule,
-    MatSelectModule,
-    CommonModule,MatFormFieldModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
