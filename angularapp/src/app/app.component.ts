2fb45b87298cda508714a243d0893118472bba18
Iamneo <techadmin@iamneo.ai>
2023-08-03 06:24:19 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -9,19 +9,25 @@ export class AppComponent {
   title = 'angularapp';
   fromCurrency=0;
   toCurrency=0;
-  amount:any;
-  result:any;
-  setSource(source:any)
-  {
-    this.fromCurrency=source;
-  }
-  setTarget(target:any)
-  {
-    this.toCurrency=target;
-  }
-  convert()
-  {
-    this.result=(this.toCurrency/this.fromCurrency)*this.amount;
-    this.result=Math.round(this.result).toFixed(2);
-  }
-}
+   amount=0;
+   num:any;
+   setSource(s:any){
+     this.fromCurrency=s;
+   } 
+ 
+   setTarget(t:any){
+     this.toCurrency=t;
+ 
+   }
+ 
+   convert(){
+    this.num=(this.toCurrency/this.fromCurrency)*this.amount;
+    this.num = Math.round(this.num).toFixed(2);
+   }
+   reset(){
+     this.amount=0;
+     this.num=0;
+     this.toCurrency=0;
+     this.fromCurrency=0;
+   }
+ }
